72ae3767d711c891df2f5ece4cd7964ea659128b
Xing <22334064@zju.edu.cn>
2025-12-17 02:24:21 +0800

    build

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/xing/roshelm_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/xing/roshelm_ws/devel;/home/xing/auh_control/devel;/home/xing/auh_ws/devel;/opt/ros/melodic'.split(';') if '/home/xing/roshelm_ws/devel;/home/xing/auh_control/devel;/home/xing/auh_ws/devel;/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/xing/roshelm_ws/devel;/home/xing/auh_ws/devel;/opt/ros/melodic'.split(';') if '/home/xing/roshelm_ws/devel;/home/xing/auh_ws/devel;/opt/ros/melodic' != '' else []
